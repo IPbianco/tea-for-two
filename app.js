23c7c9045860e9767393e6559e8c1678385c8d22
Ignacio <ignaciopanebianco@gmail.com>
2018-01-27 23:30:11 -0300

    Adds route to display template

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var express = require("express"),
     app = express(),
     path = require("path");
 
-app.get("/", function(req, res) {
-  res.status(200).send("Hello world!!!")
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.get("/:folderName", function(req, res) {
+  res.status(200).sendFile(path.join(__dirname+'/public/views/template.html'))
+})
 
 app.listen(8000, function () {
   console.log('App listening on port 8000!')
